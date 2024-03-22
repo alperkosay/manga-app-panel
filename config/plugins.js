852a0e2ae7eaf091f0047fb873d2ac48bdec84b6
Alper Koşay <83144508+Awoked@users.noreply.github.com>
2024-03-22 16:12:03 +0300

    cache

--- config/plugins.js
+++ config/plugins.js
@@ -19,4 +19,69 @@ module.exports = () => ({
       },
     },
   },
+  "rest-cache": {
+    config: {
+      provider: {
+        name: "memory",
+        options: {
+          max: 32767,
+          maxAge: 3,
+          updateAgeOnGet: false,
+        },
+      },
+      strategy: {
+        debug: true,
+        maxAge: 3600,
+        updateAgeOnGet: false,
+        contentTypes: [
+          // list of Content-Types UID to cache
+          {
+            contentType: "api::genre.genre",
+            maxAge: convertToMilliseconds(12, "h"),
+          },
+          {
+            contentType: "api::main-slide.main-slide",
+            maxAge: convertToMilliseconds(12, "h"),
+          },
+          {
+            contentType: "api::manga.manga",
+            maxAge: convertToMilliseconds(12, "h"),
+          },
+        ],
+      },
+    },
+  },
 });
+
+function convertToMilliseconds(value, unit) {
+  let milliseconds;
+
+  switch (unit) {
+    case "s":
+    case "second":
+    case "seconds":
+      milliseconds = value * 1000;
+      break;
+    case "m":
+    case "minute":
+    case "minutes":
+      milliseconds = value * 60 * 1000;
+      break;
+    case "h":
+    case "hour":
+    case "hours":
+      milliseconds = value * 60 * 60 * 1000;
+      break;
+    case "d":
+    case "day":
+    case "days":
+      milliseconds = value * 24 * 60 * 60 * 1000;
+      break;
+    default:
+      throw new Error(
+        'Invalid unit provided. Please provide "s", "m", "h", or "d".'
+      );
+  }
+
+  return milliseconds;
+}
